8ecae631cb2216475068b5c41d9b8651add7828d
BranTran <45236414+BranTran@users.noreply.github.com>
2021-12-14 15:30:10 -0600

    Change dimensions by dataset

--- js/interactive.js
+++ js/interactive.js
@@ -1,4 +1,4 @@
-var datasets = ['Used Cars', 'ATUS'];
+var datasets = ['ATUS','Used Cars'];
 d3.select('#dataset-select').selectAll("option")
 .data(datasets)
 .enter()
@@ -42,4 +42,27 @@ function magic(dimension){
         d3.select('#'+id).remove()
     }
 
+}
+
+function switchDimensions(value){
+    console.log(value);
+    var all_dimensions = [["Age Group", "Region", "Gender", "Race"],["Transmission", "Condition", "Engine"]];
+d3.select("#dimension-checkboxes").selectAll('div').remove()
+
+d3.select("#dimension-checkboxes").selectAll("p")
+.data(all_dimensions[value])
+.enter()
+.append("div")
+.append("label")
+.append("input")
+    .attr("type", "checkbox")
+    .attr("id", function(d,i) { return 'dimension'+i; })
+    .attr("onClick", "magic(this)") //DO SOMETHING WITH THIS CHANGE FUNCTION
+    .attr("for", function(d,i) { return i; })
+    .attr("value", function(d,){return d;})
+
+d3.selectAll("label")
+    .data(all_dimensions[value])
+    .append("text")
+    .text(function(d) { return d; });
 }
